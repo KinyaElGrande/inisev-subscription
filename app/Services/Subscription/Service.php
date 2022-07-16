afc48fbd8effe4d0af7dd8d83e378f7aaf10538d
kinyaelgrande <kinya.francis.mumbi@gmail.com>
2022-07-16 14:04:39 +0300

    Refactor Subscription service

--- app/Services/Subscription/Service.php
+++ app/Services/Subscription/Service.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 namespace App\Services\Subscription;
 
 use App\Models\Post;
-use App\Notifications\NewPost;
+use App\Models\PostEmailNotification;
 
 class Service
 {
-    public function sendEmailNotification(Post $post)
+    public function createPostEmailNotification(Post $post)
     {
         $post->website->subscriptions()->chunk(100, function ($subscriptions) use ($post) {
             $subscriptions->each(function ($subscription) use ($post) {
-                $subscription->user->notify(new NewPost($post));
+                PostEmailNotification::create([
+                    'post_id' => $post['id'],
+                    'user_id' => $subscription->user['id'],
+                ]);
             });
         });
+    }
+
+    public function updateSentStatus(PostEmailNotification $notification)
+    {
+        $notification->sent = true;
+        $notification->save();
     }
 }
